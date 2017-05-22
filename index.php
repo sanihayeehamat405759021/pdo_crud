39ea305518b44727fb74682a18324491118c8f60
sanihayeehamat405759021 <sani.h@yru.ac.th>
2017-05-22 16:39:44 +0700

    สร้างไฟล์ create.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-pjpinfo();
+require_once("libs/Db.php");
+$objDb = new Db();
+$db = $objDb->database;
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PDO & CRUD</title>
+  </head>
+  <body>
+
+    <?php
+    echo "Hello PDO";
+     ?>
+
+  </body>
